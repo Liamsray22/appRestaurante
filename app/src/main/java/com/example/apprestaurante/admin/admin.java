7ea6579809db7f0cbc86aef8a1f39d3c66980026
Eliam <esolis@cyberwarrior.com>
2021-04-17 16:15:06 -0400

    Vista del admin ya tiene la lista de ordenes

--- app/src/main/java/com/example/apprestaurante/admin/admin.java
+++ app/src/main/java/com/example/apprestaurante/admin/admin.java
@@ -1,16 +1,41 @@
 package com.example.apprestaurante.admin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
+import com.example.apprestaurante.Adapters.FoodAdapter;
+import com.example.apprestaurante.Adapters.OrderAdapter;
+import com.example.apprestaurante.Models.FoodModel;
+import com.example.apprestaurante.Models.OrderModel;
 import com.example.apprestaurante.R;
+import com.example.apprestaurante.databinding.ActivityAdminBinding;
+import com.example.apprestaurante.databinding.ActivityClientBinding;
+
+import java.util.ArrayList;
 
 public class admin extends AppCompatActivity {
 
+    ActivityAdminBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
+        binding = ActivityAdminBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ArrayList<OrderModel> OrderList = new ArrayList<>();
+        OrderList.add(new OrderModel("Comida", "20", "No se comia", R.drawable.ic_launcher_background));
+        OrderList.add(new OrderModel("Comida", "20", "No se comia", R.drawable.ic_launcher_background));
+        OrderList.add(new OrderModel("Comida", "20", "No se comia", R.drawable.ic_launcher_background));
+        OrderList.add(new OrderModel("Comida", "20", "No se comia", R.drawable.ic_launcher_background));
+        OrderList.add(new OrderModel("Comida", "20", "No se comia", R.drawable.ic_launcher_background));
+
+
+        OrderAdapter adapter = new OrderAdapter(OrderList, this);
+        binding.adminRecyclerView.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.adminRecyclerView.setLayoutManager(layoutManager);
     }
 }
