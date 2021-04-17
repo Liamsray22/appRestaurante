f16bd4a465c7b4e7c4de40bca7af73bf1bd04e0b
Eliam <esolis@cyberwarrior.com>
2021-04-17 15:55:59 -0400

    Activity de admins agregado y en la lista del activity cliente se ve la vista del client design

---
+++ app/src/main/java/com/example/apprestaurante/admin/admin.java
@@ -0,0 +1,16 @@
+package com.example.apprestaurante.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.apprestaurante.R;
+
+public class admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
