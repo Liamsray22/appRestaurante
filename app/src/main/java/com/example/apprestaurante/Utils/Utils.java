4491bb5da59d202067604a8e71f76c030bbcce5a
Eliam <esolis@cyberwarrior.com>
2021-04-18 17:58:15 -0400

    Fondo y vista principal del client agregados

--- app/src/main/java/com/example/apprestaurante/Utils/Utils.java
+++ app/src/main/java/com/example/apprestaurante/Utils/Utils.java
@@ -1,5 +1,5 @@
 package com.example.apprestaurante.Utils;
 
 public class Utils {
-    public static String TipoUsuario = "admin";
+    public static String TipoUsuario = "client";
 }
---
+++ app/src/main/java/com/example/apprestaurante/client/ClientMainPage.java
@@ -0,0 +1,16 @@
+package com.example.apprestaurante.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.apprestaurante.R;
+
+public class ClientMainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_main_page);
+    }
+}
