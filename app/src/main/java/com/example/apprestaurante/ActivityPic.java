eb83dd29d1a9fdc31335c10ac754cec66221eb5e
Leonardo Ismael <20186923@itla.edu.do>
2021-04-21 16:59:42 -0400

    Agregado el boton de Guardar, Codigo compila pero el app crashea

--- app/src/main/java/com/example/apprestaurante/ActivityPic.java
+++ app/src/main/java/com/example/apprestaurante/ActivityPic.java
@@ -2,14 +2,25 @@ package com.example.apprestaurante;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.icu.util.Output;
 import android.net.Uri;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class ActivityPic extends AppCompatActivity {
-
+    Button BSave;
     // One Button
     Button BSelectImage;
 
@@ -19,7 +30,7 @@ public class ActivityPic extends AppCompatActivity {
     // constant to compare
     // the activity result code
     int SELECT_PICTURE = 200;
-
+    OutputStream outputStream;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +39,39 @@ public class ActivityPic extends AppCompatActivity {
         // register the UI widgets with their appropriate IDs
         BSelectImage = findViewById(R.id.BSelectImage);
         IVPreviewImage = findViewById(R.id.IVPreviewImage);
+        BSave = findViewById(R.id.guarda);
+
+        BSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BitmapDrawable drawable = (BitmapDrawable)IVPreviewImage.getDrawable();
+                Bitmap bitmap = drawable.getBitmap();
+
+                File filepath = Environment.getExternalStorageDirectory();
+                File dir = new File(filepath.getAbsolutePath()+"/Demo/");
+                dir.mkdir();
+                File file = new File(dir, System.currentTimeMillis()+".jpg");
+                try {
+                    outputStream = new FileOutputStream(file);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                bitmap.compress(Bitmap.CompressFormat.JPEG,100, outputStream);
+                Toast.makeText(getApplicationContext(),"Saved img", Toast.LENGTH_LONG).show();
+                try {
+                    outputStream.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
 
         // handle the Choose Image button to trigger
         // the image chooser function
@@ -37,6 +81,7 @@ public class ActivityPic extends AppCompatActivity {
                 imageChooser();
             }
         });
+
     }
 
     // this function is triggered when
